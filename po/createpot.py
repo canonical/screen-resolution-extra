029aaced8912b4adccf6092c7aac26d7b09cabbe
Alberto Milone <alberto.milone@canonical.com>
2021-06-18 13:44:05 +0200

    Add changes for the 0.18 release

--- po/createpot.py
+++ po/createpot.py
@@ -3,6 +3,6 @@
 import os, sys
 
 if len(sys.argv) <= 1:
-    os.system("xgettext -k_ -kN_ -o screen-resolution-extra.pot ../ScreenResolution/ui.py")
+    os.system("xgettext -k_ -kN_ -o screen-resolution-extra.pot ../com.ubuntu.screen-resolution-extra.policy.in")
     print "Now edit messages.pot, save it as <locale>.po, and send it" \
-        " to albertomilone@alice.it.\nThanks!\n"
+        " to alberto.milone@canonical.com\nThanks!\n"
